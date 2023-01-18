d55c737d9eaf7a0354dc8d5b83b7432ff753eba4
Freddie <freddie.kaplan@edu.cmeducations.se>
2023-01-18 15:10:51 +0100

    Search-help page is finished and updated menu.

--- magic/scripts/scripts.js
+++ magic/scripts/scripts.js
@@ -4,4 +4,12 @@ $("#nav").on("click", function() {
 	$(".menu").toggleClass("active");
 });
 
+$(document).ready(function(){
+	$(".close-menu").click(function(){
+		if ($(".menu").hasClass("active")) {
+			$(".menu").removeClass("active");
+		}
+	});
+  });
+
 })
